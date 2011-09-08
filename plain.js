80ac0f0a5a2fc316395907ea2fb03e18ac0f40f3
defectus <ondrej.linek@gmail.com>
2011-09-08 11:53:03 +0000

    express enabled

--- plain.js
+++ plain.js
@@ -1,4 +1,4 @@
-var portNo = process.env.C9_PORT ? process.env.C9_PORT : process.env.PORT ? process.env.PORT : 80;
+var portNo = process.env.C9_PORT ? process.env.C9_PORT : (process.env.PORT ? process.env.PORT : 80);
 var address = process.env.C9_PORT || process.env.PORT ? "0.0.0.0" : "127.0.0.1";
 
 var http = require('http');
